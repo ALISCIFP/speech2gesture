cbb5ac7b259c0a503ea1ef79900656f601eb7afe
Zengming(Zack) Shen <szm0219@gmail.com>
2020-04-18 12:11:11 -0400

    del discrinator

--- audio_to_multiple_pose_gan/predict_audio.py
+++ audio_to_multiple_pose_gan/predict_audio.py
@@ -27,7 +27,7 @@ def main(args):
     cfg = get_config(args.config)
     pgan = PoseGAN(args, seq_len=padded_pose_shape)
     if args.checkpoint:
-        pgan.restore(args.checkpoint, scope_list=['generator', 'discriminator'])
+        pgan.restore(args.checkpoint, scope_list=['generator'])
     else:
         print "No checkpoint provided."
 
