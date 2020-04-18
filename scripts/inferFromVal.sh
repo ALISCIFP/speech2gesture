462b4f2664b93170f7c853587859d24dd30faa12
Zengming(Zack) Shen <szm0219@gmail.com>
2020-04-18 11:58:54 -0400

    up

--- scripts/inferFromVal.sh
+++ scripts/inferFromVal.sh
@@ -1,2 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python -m audio_to_multiple_pose_gan.predict_to_videos --train_csv ./s2g/train.csv --seq_len 64 -output_path_infer /export/zs/s2g/output --checkpoint ./s2g/oliver --speaker oliver -ag audio_to_pose_gans --gans 1
-
+CUDA_VISIBLE_DEVICES=0 python -m audio_to_multiple_pose_gan.predict_to_videos --train_csv ./s2g/train.csv --seq_len 64 -output_path_infer /export/zs/s2g/output --checkpoint ./s2g/oliver/ckpt-step-296700.ckp.index --speaker oliver -ag audio_to_pose_gans --gans 1
