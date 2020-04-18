0b9e83e0b5f833030e29bd7321988c8ce7213d56
Zengming(Zack) Shen <szm0219@gmail.com>
2020-04-17 23:20:32 -0400

    add

--- scripts/ExtractVal.sh
+++ scripts/ExtractVal.sh
@@ -1,2 +1,2 @@
-python -m data.train_test_data_extraction.extract_data_for_testing --base_dataset_path ./s2g -nc 4 --speaker oliver
+python -m data.train_test_data_extraction.extract_data_for_testing -c ./s2g/frames_df_10_19_19.csv --base_dataset_path ./s2g -nc 4 --speaker oliver
 
